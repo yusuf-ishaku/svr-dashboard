ce048681506bc897f71ff56d9ca37d9d11260b2f
yusuf-ishaku <kingshakzy@gmail.com>
2024-02-19 23:06:25 +0100

    correct base url added

--- src/data/apiSlices/api.js
+++ src/data/apiSlices/api.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const appApi = createApi({
     reducerPath: "appApi",
     baseQuery: fetchBaseQuery({
-            baseUrl: "https://supervibesrecords.onrender.com"
+            baseUrl: "https://super-vibes-records.onrender.com"
     }),
     tagTypes: ["User", "Audio", "Videos", "Tickets"],
     endpoints: (builder) => ({}),
